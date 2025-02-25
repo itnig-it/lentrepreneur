b6d94e4ea6579c0d28fb20d6d585812ff846b2e7
Mutsuda <mutsuda@gmail.com>
2025-02-25 18:30:13 +0100

    Prepare for Netlify deployment

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand': {
+          'light': '#f8b400',
+          'DEFAULT': '#e67e22',
+          'dark': '#d35400',
+        },
+      },
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+        'serif': ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+} 
